d056f85bc7e0195247c1eda266718d9941498bb6
joaoburatto <joaoburattoc@gmail.com>
2022-03-08 06:17:49 -0300

    Simple Lobby UI

---
+++ Assets/Scripts/SS3D/Core/Lobby/UI Helper/LobbyTabsUIHelper.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+namespace SS3D.Core.Lobby.UI_Helper
+{
+    public class LobbyTabsUIHelper : MonoBehaviour
+    {
+        [SerializeField] private GenericTabUI[] _categoryUi;
+
+        
+        
+        private void Start()
+        {
+            Destroy(this);
+            SetupGenericsTabs();
+            OnTabButtonClicked(0);
+        }
+
+        
+
+        private void SetupGenericsTabs()
+        {
+            for (int i = 0; i < _categoryUi.Length; i++)
+            {
+                int index = i;
+                _categoryUi[i].Button.onClick.AddListener(() => OnTabButtonClicked(index));
+            }
+        }
+
+        private void OnTabButtonClicked(int index)
+        {
+            Debug.Log(index);
+            foreach (GenericTabUI tab in _categoryUi)
+            {
+                tab.UpdateCategoryState(tab == _categoryUi[index]);
+            }
+        }
+    }
+}
